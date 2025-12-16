8fd36a171ae3ed3a72dc546aab3217a567d666e1
Ash Alansari <twoseasgamesstudio@gmail.com>
2025-12-16 21:07:33 +0300

    Answered clue 5 #13

--- unit-02/homework-01-sql_carmen/data/find_carmen.sql
+++ unit-02/homework-01-sql_carmen/data/find_carmen.sql
@@ -47,8 +47,12 @@ AND T.name != C.name;
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
-
+SELECT T.*, C.name AS "country_name"
+FROM city T
+JOIN country C
+	ON C.code = T.countrycode
+WHERE T.name LIKE 'Serra%'
+AND C.region = 'South America';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
