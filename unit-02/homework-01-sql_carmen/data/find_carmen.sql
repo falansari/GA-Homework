191afffb0ba5dfac729954770ece361a9d6cd4a6
Ash Alansari <twoseasgamesstudio@gmail.com>
2025-12-16 19:34:15 +0300

    answered clue 2 #13

--- unit-02/homework-01-sql_carmen/data/find_carmen.sql
+++ unit-02/homework-01-sql_carmen/data/find_carmen.sql
@@ -10,7 +10,9 @@ LIMIT 1;
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+SELECT language FROM countrylanguage
+WHERE countrycode = 'VAT'
+AND isofficial = true;
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
