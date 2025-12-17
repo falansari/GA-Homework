8b9111e852494eab5978388559a86d79b55c0600
Ash Alansari <twoseasgamesstudio@gmail.com>
2025-12-17 12:58:31 +0300

    Solved clue 7 closes #13

--- unit-02/homework-01-sql_carmen/data/find_carmen.sql
+++ unit-02/homework-01-sql_carmen/data/find_carmen.sql
@@ -81,7 +81,9 @@ AND T.id = C.capital;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+SELECT name
+FROM city
+WHERE population = 91084;
 
 
-
--- She's in ____________________________!
+-- She's in ___Santa_Monica_____________!
